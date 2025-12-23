89793195507fc64cdaa8bbd232253fde5f1f63f7
SwapnilSaha5768 <sahaswapnil5768@gmail.com>
2025-12-23 11:45:03 +0600

    update it index

--- server/index.js
+++ server/index.js
@@ -21,22 +21,18 @@ const server = http.createServer(app);
 app.use(helmet());
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
+  windowMs: 15 * 60 * 1000, 
+  max: 100
 });
 app.use(limiter);
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173", // Restrict to frontend URL
+  origin: ["http://localhost:5173", "https://connectwithme-six.vercel.app"],
   credentials: true
 }));
-app.use(express.json({ limit: '50mb' })); // Body parser must be before sanitizer
+app.use(express.json({ limit: '50mb' })); 
 
-// Security Middleware (Body-dependent)
-// app.use(mongoSanitize());
-
-// Database Connection
 connectDB();
 
 // Routes
